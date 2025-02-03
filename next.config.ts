81850215ba40bc8c58c8a1c17ee9964ed125c8f9
Michał Kubiak <michal.kubiak@my.com>
2025-02-03 01:29:25 +0100

    feat: use next image

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,14 @@ const config: NextConfig = {
       fullUrl: true,
     },
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
 };
 
 export default config;
