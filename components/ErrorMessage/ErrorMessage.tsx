ded0b34c1644ff0cc481fc6fac4c2de732266619
Michał Kubiak <michal.kubiak@my.com>
2025-02-03 01:09:23 +0100

    refactor: better a11y in pagination

--- components/ErrorMessage/ErrorMessage.tsx
+++ components/ErrorMessage/ErrorMessage.tsx
@@ -3,9 +3,13 @@ import { ErrorMessageProps } from "./ErrorMessage.types";
 
 export function ErrorMessage({ message }: ErrorMessageProps) {
   return (
-    <aside className="mx-auto mt-8 max-w-2xl rounded-lg bg-red-50 p-4 dark:bg-red-900/10">
+    <aside
+      className="mx-auto mt-8 max-w-2xl rounded-lg bg-red-50 p-4 dark:bg-red-900/10"
+      role="alert"
+      aria-live="assertive"
+    >
       <div className="flex">
-        <div className="flex-shrink-0">
+        <div className="shrink-0">
           <XCircleIcon className="h-5 w-5 text-red-400" aria-hidden="true" />
         </div>
         <div className="ml-3">
